3d9936a300a29a572ce985aa52579ab93a79da92
Ruy Rodrigues <ruy.rodrigues@edu.unifil.br>
2021-09-10 11:20:28 -0300

    first commit

---
+++ iOS - BRQ Invest/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  iOS - BRQ Invest
+//
+//  Created by user on 09/09/21.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var moedaLabel: UILabel!
+    @IBOutlet weak var variacaoLabel: UILabel!
+    
+}
